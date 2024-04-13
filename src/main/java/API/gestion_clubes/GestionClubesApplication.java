47807ce3742eaedddb5579c644ce13cc9f20f989
aquesada01 <jquesada214@gmail.com>
2024-04-13 12:28:24 -0500

    Carga inicial de proyecto y configuracion application.properties

---
+++ src/main/java/API/gestion_clubes/GestionClubesApplication.java
@@ -0,0 +1,13 @@
+package API.gestion_clubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClubesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionClubesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/gestion_clubes/GestionClubesApplicationTests.java
@@ -0,0 +1,13 @@
+package API.gestion_clubes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClubesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
